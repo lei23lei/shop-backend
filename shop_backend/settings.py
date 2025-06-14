9a2f9df518eea7ebae34ba4a4bf39d83cc225329
peter <lei23lei91@gmail.com>
2025-06-13 22:43:48 -0400

    set login token one month

--- shop_backend/settings.py
+++ shop_backend/settings.py
@@ -224,7 +224,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=24 * 30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
